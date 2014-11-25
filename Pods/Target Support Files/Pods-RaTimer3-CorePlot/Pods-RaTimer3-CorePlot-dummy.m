f74b95b5bd7bc71bb525beef2dc39476dd657704
Riku Oja <riku.oja@kapsi.fi>
2014-11-25 15:18:23 +0200

    Kääntyy podien kanssa, CorePlot lisätty

---
+++ Pods/Target Support Files/Pods-RaTimer3-CorePlot/Pods-RaTimer3-CorePlot-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RaTimer3_CorePlot : NSObject
+@end
+@implementation PodsDummy_Pods_RaTimer3_CorePlot
+@end
---
+++ Pods/Target Support Files/Pods-RaTimer3/Pods-RaTimer3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RaTimer3 : NSObject
+@end
+@implementation PodsDummy_Pods_RaTimer3
+@end
